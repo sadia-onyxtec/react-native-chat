5c298f2284f1c558128ace56e8624a5891dc80a9
sadia-onyxtec <sc@onyxtec.co>
2023-02-21 14:12:17 +0500

    integrating notifications in the app

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import HomeScreen from './src/screens/HomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import ChatScreen from './src/screens/ChatScreen';
 import SignUpScreen from './src/screens/SignUpScreen';
-
+import PushNotification from "react-native-push-notification";
 
 const Stack = createStackNavigator();
 const AuthenticatedUserContext = createContext({});
@@ -92,6 +92,34 @@ function RootNavigator() {
 }
 
 export default function App() {
+  useEffect(() => {
+    PushNotification.configure({
+      // (optional) Called when Token is generated (iOS and Android)
+      onRegister: function (token) {
+        console.log("TOKEN:", token);
+      },
+
+      // (required) Called when a remote or local notification is opened or received
+      onNotification: function (notification) {
+        console.log("NOTIFICATION:", notification);
+
+        // process the notification here
+
+        // required on iOS only 
+        // notification.finish(PushNotificationIOS.FetchResult.NoData);
+      },
+      // Android only
+      senderID: "877788395377",
+      // iOS only
+      permissions: {
+        alert: true,
+        badge: true,
+        sound: true
+      },
+      popInitialNotification: true,
+      requestPermissions: true
+    });
+  }, []);
   return (
     <AuthenticatedUserProvider>
       <RootNavigator />
